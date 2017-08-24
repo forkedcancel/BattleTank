d30e2df70bd83192c834889e65eb5eaab192f551
William Rose <will@williamro.se>
2017-08-23 22:51:06 -0400

    Start focusing on Elevate method. Working on migrating to 4.17

--- Source/BattleTank/Private/TankBarrel.cpp
+++ Source/BattleTank/Private/TankBarrel.cpp
@@ -1,9 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankBarrel.h"
+#include "Engine/World.h"
 
-
-
-void UTankBarrel::Elevate(float DegreesPerSecond) {
-
+void UTankBarrel::Elevate(float RelativeSpeed) {
+    auto Time = GetWorld()->GetTimeSeconds();
+    UE_LOG(LogTemp, Warning, TEXT("%f: Barrel->Elevate() called ate speed %f"), Time, RelativeSpeed);
 }
