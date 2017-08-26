b448d8d89d99f8f1e77809d10952389527c1c60b
William Rose <will@williamro.se>
2017-08-26 18:16:42 -0400

    Add SetThrottle to TankTrack and bind via blueprint

--- Source/BattleTank/Private/Projectile.cpp
+++ Source/BattleTank/Private/Projectile.cpp
@@ -30,7 +30,7 @@ void AProjectile::Tick(float DeltaTime)
 
 void AProjectile::LaunchProjectile(float Speed) {
 	auto Time = GetWorld()->GetTimeSeconds();
-	UE_LOG(LogTemp, Warning, TEXT("%f: Tank has fired at %f"), Time, Speed);
+
 	ProjectileMovement->SetVelocityInLocalSpace(FVector::ForwardVector * Speed);
 	ProjectileMovement->Activate();
 }
--- Source/BattleTank/Public/TankTrack.h
+++ Source/BattleTank/Public/TankTrack.h
@@ -7,7 +7,7 @@
 #include "TankTrack.generated.h"
 
 /**
- * 
+ * TankTrack is used to set maximum driving force, and to apply forces to the tank.
  */
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class BATTLETANK_API UTankTrack : public UStaticMeshComponent
@@ -15,6 +15,10 @@ class BATTLETANK_API UTankTrack : public UStaticMeshComponent
 	GENERATED_BODY()
 	
 	
-	
+public:
+
+	// Sets a throttle between -1 and +1
+	UFUNCTION(BlueprintCallable, Category = Input)
+	void SetThrottle(float Throttle);
 	
 };
